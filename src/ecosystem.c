7f6baa18992d541f2f401553e351e0026f14a7c6
bl33h <per21371@uvg.edu.gt>
2024-08-07 21:04:32 -0600

    moving & resources consumption functions placed, the energy print per species is not accurate

--- src/ecosystem.c
+++ src/ecosystem.c
@@ -23,6 +23,8 @@ typedef struct {
     int carnivores;
     int herbivores_consumed;
     int carnivores_consumed;
+    int herbivore_energy;
+    int carnivore_energy;
 } Cell;
 
 Cell grid[GRID_SIZE][GRID_SIZE];
@@ -36,6 +38,8 @@ void initializeEcosystem() {
             grid[i][j].carnivores = rand() % 20 + 5;
             grid[i][j].herbivores_consumed = 0;
             grid[i][j].carnivores_consumed = 0;
+            grid[i][j].herbivore_energy = 0;
+            grid[i][j].carnivore_energy = 0;
         }
     }
 }
@@ -66,17 +70,61 @@ void reproduce(int i, int j, int species) {
     }
 }
 
+// herbivores gain 5 units of energy per plant consumed
 void herbivoresConsumePlants(int i, int j) {
     if (grid[i][j].plants > 0 && grid[i][j].herbivores > 0) {
         grid[i][j].plants--;
         grid[i][j].herbivores_consumed++;
+        grid[i][j].herbivore_energy += 5; 
     }
 }
 
+// carnivores gain 10 units of energy per herbivore consumed
 void carnivoresConsumeHerbivores(int i, int j) {
     if (grid[i][j].herbivores > 0 && grid[i][j].carnivores > 0) {
         grid[i][j].herbivores--;
         grid[i][j].carnivores_consumed++;
+        grid[i][j].carnivore_energy += 10; 
+    }
+}
+
+void moveHerbivores(int i, int j) {
+    int directions[8][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+    int best_move = -1;
+    int max_plants = 0;
+    for (int d = 0; d < 8; d++) {
+        int ni = i + directions[d][0];
+        int nj = j + directions[d][1];
+        if (isWithinBounds(ni, nj) && grid[ni][nj].carnivores == 0 && grid[ni][nj].plants > max_plants) {
+            max_plants = grid[ni][nj].plants;
+            best_move = d;
+        }
+    }
+
+    // move to the cell with the most plants and no carnivores
+    if (best_move != -1) { 
+        grid[i + directions[best_move][0]][j + directions[best_move][1]].herbivores++;
+        grid[i][j].herbivores--;
+    }
+}
+
+void moveCarnivores(int i, int j) {
+    int directions[8][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+    int best_move = -1;
+    int max_herbivores = 0;
+    for (int d = 0; d < 8; d++) {
+        int ni = i + directions[d][0];
+        int nj = j + directions[d][1];
+        if (isWithinBounds(ni, nj) && grid[ni][nj].herbivores > max_herbivores) {
+            max_herbivores = grid[ni][nj].herbivores;
+            best_move = d;
+        }
+    }
+
+    // move to the cell with the most herbivores
+    if (best_move != -1) { 
+        grid[i + directions[best_move][0]][j + directions[best_move][1]].carnivores++;
+        grid[i][j].carnivores--;
     }
 }
 
@@ -85,6 +133,8 @@ void updateEcosystem() {
         #pragma omp parallel for collapse(2)
         for (int i = 0; i < GRID_SIZE; i++) {
             for (int j = 0; j < GRID_SIZE; j++) {
+                moveHerbivores(i, j);
+                moveCarnivores(i, j);
                 grid[i][j].plants = max(0, grid[i][j].plants + 5 - grid[i][j].herbivores / 5);
                 grid[i][j].herbivores = max(0, grid[i][j].herbivores + (grid[i][j].plants / 10) - grid[i][j].carnivores / 5);
                 grid[i][j].carnivores = max(0, grid[i][j].carnivores + (grid[i][j].herbivores / 10));
@@ -97,23 +147,32 @@ void updateEcosystem() {
                 reproduce(i, j, 2);
             }
         }
+        
         #pragma omp barrier
 
         int totalPlants = 0, totalHerbivores = 0, totalCarnivores = 0;
+        int totalHerbivoreEnergy = 0, totalCarnivoreEnergy = 0;
+
         printf("\nTick %d:\n", tick);
         for (int i = 0; i < GRID_SIZE; i++) {
             for (int j = 0; j < GRID_SIZE; j++) {
                 totalPlants += grid[i][j].plants;
                 totalHerbivores += grid[i][j].herbivores;
                 totalCarnivores += grid[i][j].carnivores;
+                totalHerbivoreEnergy += grid[i][j].herbivore_energy;
+                totalCarnivoreEnergy += grid[i][j].carnivore_energy;
+
+                // the dominant species in the cell (just for the beginning)
                 char dominant = 'P';
                 if (grid[i][j].herbivores > grid[i][j].plants && grid[i][j].herbivores > grid[i][j].carnivores) dominant = 'H';
-                else if (grid[i][j].carnivores > grid[i][j].plants && grid[i][j].carnivores > grid[i][j].herbivores) dominant = 'C';
+                if (grid[i][j].carnivores > grid[i][j].plants && grid[i][j].carnivores > grid[i][j].herbivores) dominant = 'C';
                 printf("%c ", dominant);
             }
             printf("\n");
         }
         printf("Plants: %d, Herbivores: %d, Carnivores: %d\n", totalPlants, totalHerbivores, totalCarnivores);
+        printf("* Total Herbivore Energy: %d\n", totalHerbivoreEnergy);
+        printf("* Total Carnivore Energy: %d\n", totalCarnivoreEnergy);
     }
 }
 
